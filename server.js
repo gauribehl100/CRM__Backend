b51f1444630de5a1d7b5c074fb7f7c91c56cb164
Gauri Behl <140520216+gauribehl100@users.noreply.github.com>
2025-06-03 14:06:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const { errorHandler } = require('./middleware/errorHandler');
 const { logger } = require('./utils/logger');
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Security middleware
 app.use(helmet());
